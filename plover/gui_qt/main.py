3f4506429b6b124f9ac3910be9e7bf549ba2b78c
Benoit Pierre <benoit.pierre@gmail.com>
2016-08-11 10:49:36 +0200

    gui_qt: fix initialization if dbus is not available

--- plover/gui_qt/main.py
+++ plover/gui_qt/main.py
@@ -88,21 +88,25 @@ def show_error(title, message):
 
 def main(config):
 
-    handler = None
+    use_qt_notifications = True
+    handler_class = None
     try:
         if sys.platform.startswith('linux'):
             from plover.oslayer.log_dbus import DbusNotificationHandler
-            handler = DbusNotificationHandler
+            handler_class = DbusNotificationHandler
         elif sys.platform.startswith('darwin'):
             from plover.oslayer.log_osx import OSXNotificationHandler
-            handler = OSXNotificationHandler
+            handler_class = OSXNotificationHandler
     except Exception:
         log.info('could not import platform gui log', exc_info=True)
-    if handler is not None:
-        log.add_handler(handler())
-        use_qt_notifications = False
-    else:
-        use_qt_notifications = True
+    if handler_class is not None:
+        try:
+            handler = handler_class()
+        except Exception:
+            log.info('could not initialize platform gui log', exc_info=True)
+        else:
+            log.add_handler(handler)
+            use_qt_notifications = False
 
     # Setup internationalization support.
     install_gettext()
