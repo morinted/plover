7319e1d5cb5ab51a6ae17b3e672ab5250b419694
Benoit Pierre <benoit.pierre@gmail.com>
2016-08-14 11:23:29 +0200

    gui_qt: rework suggestions widget presentation

--- plover/gui_qt/suggestions_widget.py
+++ plover/gui_qt/suggestions_widget.py
@@ -2,8 +2,6 @@ from PyQt5.QtGui import (
     QFont,
     QTextCursor,
     QTextCharFormat,
-    QTextFrameFormat,
-    QTextListFormat,
 )
 from PyQt5.QtWidgets import QWidget
 
@@ -17,20 +15,14 @@ class SuggestionsWidget(QWidget, Ui_SuggestionsWidget):
     STYLE_TRANSLATION, STYLE_STROKES = range(2)
 
     # Anatomy of the text document:
-    # - "root" frame:
-    #  - 0+ "suggestions" frames
-    #   - 1+ "translation" frames
-    #    - 1-10 "strokes" frames
+    # - "root":
+    #  - 0+ "suggestions" blocks
+    #   - 1+ "translation" blocks
+    #    - 1-10 "strokes" blocks
 
     def __init__(self):
         super(SuggestionsWidget, self).__init__()
         self.setupUi(self)
-        self._suggestions_frame_format = QTextFrameFormat()
-        self._suggestions_frame_format.setBorderStyle(QTextFrameFormat.BorderStyle_Inset)
-        self._suggestions_frame_format.setBorder(1)
-        self._suggestions_char_format = QTextCharFormat()
-        self._translation_frame_format = QTextFrameFormat()
-        self._translation_frame_format.setLeftMargin(4.0)
         self._translation_char_format = QTextCharFormat()
         self._strokes_char_format = QTextCharFormat()
         self._strokes_char_format.font().setStyleHint(QFont.Monospace)
@@ -39,22 +31,20 @@ class SuggestionsWidget(QWidget, Ui_SuggestionsWidget):
         before_height = self.suggestions.document().size().height()
         cursor = self.suggestions.textCursor()
         cursor.movePosition(QTextCursor.Start)
-        frame = cursor.insertFrame(self._suggestions_frame_format)
         for suggestion in suggestion_list:
-            child_frame = frame.lastCursorPosition().insertFrame(self._translation_frame_format)
-            cursor = child_frame.lastCursorPosition()
+            cursor.insertBlock()
             cursor.setCharFormat(self._translation_char_format)
             cursor.block().setUserState(self.STYLE_TRANSLATION)
             cursor.insertText(escape_translation(suggestion.text) + u':')
             if not suggestion.steno_list:
                 cursor.insertText(u' ' + _('no suggestions'))
                 continue
-            cursor.insertList(QTextListFormat.ListDisc)
             for strokes_list in suggestion.steno_list[:10]:
+                cursor.insertBlock()
                 cursor.setCharFormat(self._strokes_char_format)
                 cursor.block().setUserState(self.STYLE_STROKES)
-                cursor.insertText(u'/'.join(strokes_list) + u'\n')
-            cursor.deletePreviousChar()
+                cursor.insertText(u'   ' + u'/'.join(strokes_list))
+        cursor.insertText('\n')
         # Keep current position when not at the top of the document.
         scrollbar_value = self.suggestions.verticalScrollBar().value()
         if scrollbar_value != 0:
