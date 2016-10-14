07e17fe3e7bd8db0b139234742ed74920c4289e9
Ted Morin <theodore.morin@clearwaterclinical.com>
2016-10-13 23:34:17 -0400

    Add edge logic to stroke reading

--- plover/machine/procat.py
+++ plover/machine/procat.py
@@ -39,7 +39,9 @@ class ProCAT(SerialStenotypeBase):
 
             # Grab data from the serial port.
             raw = self.serial_port.read(BYTES_PER_STROKE)
-            if not raw or raw[0] == 1:
+            if (not raw or len(raw) < BYTES_PER_STROKE or
+                    # First bit should be 0, last byte should be FF.
+                    raw[0] & 128 or raw[3] != 0xFF):
                 continue
 
             # Convert the raw to a list of steno keys.
