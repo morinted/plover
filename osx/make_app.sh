a73ccaf1a9bf6dc93dfe51a2ceaafcb3ca31a17c
Ted Morin <morinted@gmail.com>
2017-02-25 21:39:14 -0500

    Launchable app!

--- osx/make_app.sh
+++ osx/make_app.sh
@@ -4,17 +4,19 @@ function make_app {
   osx_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
   python3_location=$(greadlink -f $(which python3))
-  pip3=$(greadlink -f $(which pip3))
-  pyvenv3=$(greadlink -f $(which pyvenv))
+  echo "Python 3 location: $python3_location"
+
   python3_bin_dir=$(dirname $python3_location)
   python3_dir=$(dirname $python3_bin_dir)
 
   py_version=$(basename $python3_dir)
+  echo "Python version: $py_version"
 
   plover_dir=$(dirname $osx_dir)
   app_dir=$plover_dir/build/Plover.app
 
   target_python=python${py_version}
+  python3_lib=$python3_dir/lib/$target_python
   target_pip=pip${py_version}
   target_pyvenv=pyvenv-${py_version}
 
@@ -32,22 +34,8 @@ function make_app {
   pip3 install virtualenv
 
   virtualenv -p python3 --always-copy $target_dir/plover-env
+  cp -RPn $python3_lib $target_dir/plover-env/lib
   source $target_dir/plover-env/bin/activate
-
-# -----------------
-
-  # #sed -i '' "1s/.*/#!.\/$target_python/" $target_dir/$target_pip
-
-  # Make site-packages Local Again
-  # Replace symlink with real folder
-  rm $target_dir/lib/$target_python/site-packages
-  mkdir $target_dir/lib/$target_python/site-packages
-  # Add sitecustomize.py that will add the folder to site
-  cp $plover_dir/osx/sitecustomize.py $target_dir/lib/$target_python/sitecustomize.py
-  # Disable user site-packages by changing a constant in site.py
-  sed -ie 's/ENABLE_USER_SITE = None/ENABLE_USER_SITE = False/g' $target_dir/lib/$target_python/site.py
-
-# -----------------
 
   plover_wheel=$(find $plover_dir/dist -name '*.whl')
   pip install $plover_wheel -c $plover_dir/requirements_constraints.txt
@@ -60,28 +48,23 @@ function make_app {
   # Make scripts relocatable
   virtualenv -p python3 --relocatable $target_dir/plover-env
 
-  cd $target_dir
+  
+  # Fix dylibs
+  echo 'Running macholib'
+  pip3 install macholib
+  python -m macholib standalone $app_dir
 
-  target_dylib=$target_dir/plover-env/lib${target_python}.dylib
-  cp $python3_dir/Python $target_dylib
-
-  # Change binary to point at our own dylib
-  echo 'Fixing Python dylib reference...'
-  chmod 777 $target_dir/plover-env/.Python
-  install_name_tool -id @executable_path/lib${target_python}.dylib $target_dir/plover-env/.Python
-
-  # Change libpython similarly
-  echo 'Fixing dylib self-reference...'
-  chmod 777 $target_dylib
-  install_name_tool -id @executable_path/lib${target_python}.dylib $target_dylib
-
-  # Copy icon
   echo 'Copying icon...'
   cp $osx_dir/plover.icns $app_dir/Contents/Resources/plover.icns
 
+  echo 'Copying launcher...'
+  cp $osx_dir/plover_launcher $app_dir/Contents/MacOS/Plover
+  chmod +x $app_dir/Contents/MacOS/Plover
+
+
   echo 'Creating PLists'
   # This allows notifications to identify as from Plover.
-  /usr/libexec/PlistBuddy -c 'Add :CFBundleIdentifier string "org.openstenoproject.plover"' $target_dir/plover-enc/bin/Info.plist
+  /usr/libexec/PlistBuddy -c 'Add :CFBundleIdentifier string "org.openstenoproject.plover"' $target_dir/plover-env/bin/Info.plist
 
   # Setup PList for our .app
   /usr/libexec/PlistBuddy -c 'Add :CFBundleDevelopmentRegion string "en"' $app_dir/Contents/Info.plist
@@ -89,7 +72,7 @@ function make_app {
   /usr/libexec/PlistBuddy -c 'Add :CFBundleIdentifier string "org.openstenoproject.plover"' $app_dir/Contents/Info.plist
   /usr/libexec/PlistBuddy -c 'Add :CFBundleName string "Plover"' $app_dir/Contents/Info.plist
   /usr/libexec/PlistBuddy -c 'Add :CFBundleDisplayName string "Plover"' $app_dir/Contents/Info.plist
-  /usr/libexec/PlistBuddy -c 'Add :CFBundleExecutable string "Frameworks/plover-env/bin/plover"' $app_dir/Contents/Info.plist
+  /usr/libexec/PlistBuddy -c 'Add :CFBundleExecutable string "MacOS/Plover"' $app_dir/Contents/Info.plist
   /usr/libexec/PlistBuddy -c 'Add :CFBundlePackageType string "APPL"' $app_dir/Contents/Info.plist
   /usr/libexec/PlistBuddy -c 'Add :CFBundleShortVersionString string "1.2.3"' $app_dir/Contents/Info.plist
   /usr/libexec/PlistBuddy -c 'Add :CFBundleVersion string "1.2.3"' $app_dir/Contents/Info.plist
@@ -99,6 +82,8 @@ function make_app {
   /usr/libexec/PlistBuddy -c "Add :NSHumanReadableCopyright string \"$copyright\"" $app_dir/Contents/Info.plist
   /usr/libexec/PlistBuddy -c 'Add :NSPrincipalClass string "NSApplication"' $app_dir/Contents/Info.plist
   /usr/libexec/PlistBuddy -c 'Add :NSAppSleepDisabled bool true' $app_dir/Contents/Info.plist
+
+  deactivate
 }
 
 make_app
